4d5e53f369b20796c8ff3f767edc4ad5b1063eb8
Duong Hoang <hoangduong01hpp@gmail.com>
2021-09-27 01:05:15 +0700

    test app

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const car = {
+    name: 'Mercedes',
+    model: 'E300',
+    weight = '800kg',
+    color = 'Red'
+}
+
+car.color = 'black'
+car.weight = '1000kg'
+
+console.log(car.color)
+console.log(car.weight)
